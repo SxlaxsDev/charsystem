39c9cc422c7724e9dd5f4b23228a261e5f518b5c
SxlaxsDev <59236047+SxlaxsDev@users.noreply.github.com>
2019-12-27 18:58:30 +0100

    Updated sh_sys.lua
    Fixed a bug, that players can change their job via /jobcommand

--- simplechar-your-charactersystem/charsys/lua/charsystem/sh_sys.lua
+++ simplechar-your-charactersystem/charsys/lua/charsystem/sh_sys.lua
@@ -2,4 +2,8 @@ timer.Simple(0, function()
 	DarkRP.removeChatCommand("forcerpname")
 	DarkRP.removeChatCommand("freerpname")
 	concommand.Remove("rp_resetallmoney")
+        for k,v in pairs(RPExtraTeams) do
+        concommand.Remove(v.command)
+        end
+     
 end)
