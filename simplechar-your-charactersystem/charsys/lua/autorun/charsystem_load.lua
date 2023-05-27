337cd4584cc34f0b26f5afcbd1202ad9f220c773
Fabian <52082025+Austria7@users.noreply.github.com>
2023-05-27 21:56:21 +0200

    change charsystem_load.lua formatting

--- simplechar-your-charactersystem/charsys/lua/autorun/charsystem_load.lua
+++ simplechar-your-charactersystem/charsys/lua/autorun/charsystem_load.lua
@@ -3,12 +3,12 @@ if SERVER then
 	include("charsystem/sv_commands.lua")
 	include("charsystem/sh_sys.lua")
 	include("charsystem/sv_main.lua")
-        include("charsystem/sv_adminmenu.lua")
+    include("charsystem/sv_adminmenu.lua")
 	AddCSLuaFile("charsystem/sh_config.lua")
 	AddCSLuaFile("charsystem/sh_sys.lua")
 	AddCSLuaFile("charsystem/cl_menu.lua")
-        AddCSLuaFile("charsystem/cl_adminmenu.lua")
-        AddCSLuaFile("charsystem/cl_fonts.lua")
+    AddCSLuaFile("charsystem/cl_adminmenu.lua")
+    AddCSLuaFile("charsystem/cl_fonts.lua")
 
 end
 
@@ -17,6 +17,5 @@ if CLIENT then
 	include("charsystem/sh_sys.lua")
 	include("charsystem/cl_menu.lua")
 	include("charsystem/cl_fonts.lua")
-        include("charsystem/cl_adminmenu.lua")
+    include("charsystem/cl_adminmenu.lua")
 end
-
