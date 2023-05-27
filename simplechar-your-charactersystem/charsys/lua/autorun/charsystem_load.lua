7fcc50dfd6903ec1ac173a94cb154ab5b2fc083d
Aron Rohr <aron.rohr@web.de>
2023-05-28 00:38:50 +0200

    Change loading structure add print

--- simplechar-your-charactersystem/charsys/lua/autorun/charsystem_load.lua
+++ simplechar-your-charactersystem/charsys/lua/autorun/charsystem_load.lua
@@ -1,21 +1,21 @@
-if SERVER then
-	include("charsystem/sh_config.lua")
-	include("charsystem/sv_commands.lua")
-	include("charsystem/sh_sys.lua")
-	include("charsystem/sv_main.lua")
-    include("charsystem/sv_adminmenu.lua")
-	AddCSLuaFile("charsystem/sh_config.lua")
-	AddCSLuaFile("charsystem/sh_sys.lua")
-	AddCSLuaFile("charsystem/cl_menu.lua")
-    AddCSLuaFile("charsystem/cl_adminmenu.lua")
-    AddCSLuaFile("charsystem/cl_fonts.lua")
+AddCSLuaFile("charsystem/sh_config.lua")
+include("charsystem/sh_config.lua")
+AddCSLuaFile("charsystem/sh_sys.lua")
+include("charsystem/sh_sys.lua")
 
+if SERVER then
+	include("charsystem/sv_commands.lua")
+	include("charsystem/sv_main.lua")
+	include("charsystem/sv_adminmenu.lua")
+	AddCSLuaFile("charsystem/cl_menu.lua")
+	AddCSLuaFile("charsystem/cl_adminmenu.lua")
+	AddCSLuaFile("charsystem/cl_fonts.lua")
 end
 
 if CLIENT then
-	include("charsystem/sh_config.lua")
-	include("charsystem/sh_sys.lua")
 	include("charsystem/cl_menu.lua")
 	include("charsystem/cl_fonts.lua")
-    include("charsystem/cl_adminmenu.lua")
+	include("charsystem/cl_adminmenu.lua")
 end
+
+MsgC(Color(255,123,0), "[CharSystem] ", Color(255,255,255), "Loaded!\n")
